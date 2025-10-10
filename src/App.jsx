e60eb0061c9bbfb31c9b814ce92a789a7e4061a1
Laksumi Soma <92606166+sumi2202@users.noreply.github.com>
2025-10-10 16:03:50 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+
+function App() {
+  
+  return ( 
+  <div className="w-full flex h-screen justify-center items-center p-4">
+    <div className="border-[1px] border-gray-700 max-w-6xl w-full min-h-[600px] rounded-lg">
+      {/* Header */}
+      <div className="">
+        <div>
+          <p>signed in as name...</p>
+        </div>
+      </div>
+    </div>
+  </div>
+  );
+    
+}
+
+export default App
